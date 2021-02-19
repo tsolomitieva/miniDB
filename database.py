eddbed3e0c652ab1e32fb2d3c7012d485f0caa3e
eva <tsolomitieva@gmail.com>
2021-02-19 22:40:21 +0200

    right join

--- database.py
+++ database.py
@@ -599,3 +599,20 @@ class Database:
                  return res
              else:
                  res.show()
+
+    def right_join(self, left_table_name, right_table_name, condition, save_as=None, return_object=False):
+
+          self.load(self.savedir)
+          if self.is_locked(left_table_name) or self.is_locked(right_table_name):
+              print(f'Table/Tables are currently locked')
+              return
+
+          res = self.tables[left_table_name]._right_join(self.tables[right_table_name], condition)
+          if save_as is not None:
+              res._name = save_as
+              self.table_from_object(res)
+          else:
+              if return_object:
+                  return res
+              else:
+                  res.show()
